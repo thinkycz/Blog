1a9c4101e27f5da06af09b5935a1e3ceca9e166e
thinkycz <me@thinky.cz>
2014-12-12 00:14:06 +0100

    Login controller added

---
+++ src/Cvut/Fit/BiWt1/Blog/ApplicationBundle/Tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Cvut\Fit\BiWt1\Blog\ApplicationBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+    }
+
+}
